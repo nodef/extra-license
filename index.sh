81d4d98642b84cdd8c12f1b996cace1e5694412a
Subhajit Sahu <wolfram77@gmail.com>
2018-09-08 12:47:19 +0530

    add install script

---
+++ index.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# present script directory
+psd() {
+  z="${BASH_SOURCE[0]}"
+  if [ -h "$z" ]; then z="$(readlink "$z")"; fi
+  cd "$(dirname "$0")" && cd "$(dirname "$z")" && pwd
+}
+
+
+# run command
+dp0="$(psd)/"
+node "$dp0." "$@"
