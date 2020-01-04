4c60f45f40b6065694ce01a8586413e03a332cf4
Subhajit Sahu <wolfram77@gmail.com>
2020-01-04 18:20:57 +0530

    can rename deprecated_ to plain

--- build2.js
+++ build2.js
@@ -1,20 +1,35 @@
-const http = require('https');
 const path = require('path');
 const fs = require('fs');
+const cp = require('child_process');
+
+const OPTIONS = {
+  stdio: [0, 1, 2]
+};
 
 
 
-// Download file from URL (redirect allowed).
-function download(url, filename) {
-  return new Promise((fres, frej) => {
-    var req = http.get(url, (res) => {
-      var code = res.statusCode;
-      if (code>=300 && code<400) return download(res.headers.location, filename).then(fres);
-      if (code>=400) return frej(res.statusCode);
-      filename = filename || path.basename(url);
-      var file = fs.createWriteStream(filename);
-      res.pipe(file).on('close', () => fres(filename));
-    });
-    req.on('error', () => fres(0));
-  });
+/**
+ * Rename files in a directory.
+ * @param {string} dir path to directory containing files
+ * @param {RegExp} match which files to rename
+ * @param {RegExp} from search regex for filename
+ * @param {string} to value for filename
+ */
+function renameFiles(dir, match, from, to) {
+  for(var f of fs.readdirSync(dir)) {
+    if(!match.test(f)) continue;
+    var src = path.join(dir, f);
+    var dst = path.join(dir, f.replace(from, to));
+    fs.renameSync(src, dst);
+  }
 }
+
+
+function main() {
+  cp.execSync('git clone --depth=1 https://github.com/spdx/license-list-data', OPTIONS);
+  cp.execSync('mv license-list-data/text text', OPTIONS);
+  renameFiles('text', /^deprecated_/, /^deprecated_/, '');
+  cp.execSync('rm -rf license-list-data', OPTIONS);
+  // cp.execSync('git clone --depth=1 https://github.com/github/choosealicense.com', OPTIONS);
+}
+main();
